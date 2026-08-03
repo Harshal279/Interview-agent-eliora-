9a17f07c66250815a020c49462deedc39c999138
Harshal Ghoradkar <116966659+Harshal279@users.noreply.github.com>
2026-08-03 11:02:25 +0530

    Update llm.py

--- app/llm.py
+++ app/llm.py
@@ -3,7 +3,7 @@ import os
 
 def get_openai_client():
     return OpenAI(
-        api_key="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",
+        api_key="gsk_",
         base_url="https://api.groq.com/openai/v1",
     )
 
