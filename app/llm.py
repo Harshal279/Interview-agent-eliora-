06ea1778982f56621667054254eff71503ffc233
Harshal Ghoradkar <116966659+Harshal279@users.noreply.github.com>
2026-08-03 11:01:24 +0530

    question generator

--- app/llm.py
+++ app/llm.py
@@ -1,8 +1,9 @@
 from openai import OpenAI
 import os
-client = OpenAI(
-    api_key=os.environ.get("GROQ_API_KEY"),
-    base_url="https://api.groq.com/openai/v1",
-)
 
-    
+def get_openai_client():
+    return OpenAI(
+        api_key="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",
+        base_url="https://api.groq.com/openai/v1",
+    )
+
